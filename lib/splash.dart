249e90a4d4413e7edca0371350fa8de806d2ce30
Anitha P Benny <anithapbenny02@gmail.com>
2024-03-15 15:17:13 +0530

    update

--- lib/splash.dart
+++ lib/splash.dart
@@ -11,12 +11,9 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
     // Delay the navigation by 2 seconds using Future.delayed
-
     Future.delayed(const Duration(seconds: 2), () {
       // Use pushReplacementNamed once the context is available
-
       Navigator.pushReplacementNamed(context, '/loginPage');
     });
   }
@@ -24,10 +21,31 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:
+          Color.fromARGB(255, 4, 30, 51), // Change background color to blue
       body: Center(
-        child: Text(
-          'FarmSage',
-          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius:
+                  BorderRadius.circular(100), // Adjust the value as needed
+              child: Image.asset(
+                'img/splash.png', // Replace this with your image path
+                width: 200, // Adjust as needed
+                height: 200, // Adjust as needed
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'FarmSage',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white, // Change font color to blue
+              ),
+            ),
+          ],
         ),
       ),
     );
